4a42f1b286cf263d5330b922369a17550b9db987
fancy <1186710960@qq.com>
2021-05-24 18:39:24 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
+    ...base,
     mode: 'development',
-    entry: './src/index.js',
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: '范范',
-        template: './src/assets/index.html'
-    })],
     module: {
         rules: [{
             test: /\.css$/i, //正则，以.css结尾
-            use: ["style-loader", "css-loader"],
+            use: ["style-loader", "css-loader"]
         }, ],
     },
 };
